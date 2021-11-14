0f276e22b99342e244be31e6c0e04ea0dbf422dc
ASTIC <github@cl-mail.eu>
2021-11-14 12:52:39 +0100

    Update API and workings

--- VotingRewardMod/Properties/AssemblyInfo.cs
+++ VotingRewardMod/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ASTIC/TC (adaped from Mortlath)")]
 [assembly: AssemblyProduct("VotingRewardMod")]
-[assembly: AssemblyCopyright("Copyright ©2021")]
+[assembly: AssemblyCopyright("ASTIC/TC ©2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
